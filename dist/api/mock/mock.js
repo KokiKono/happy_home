5e2803e7f707002766f7a1c874830f64b8b18e9f
kokikono <kk.k.0421@outlook.jp>
2017-10-20 20:47:59 +0900

    update:不要なファイルを削除

--- dist/api/mock/mock.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.sample = function (req, res) {
-    var user = {};
-    user.username = req.param('username');
-    user.password = req.param('password');
-    res.json(user);
-};
-//# sourceMappingURL=mock.js.map
