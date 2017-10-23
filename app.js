1b52c8af1968728f3e02be72c80594801317d3b1
Tester <yankuma4@gmail.com>
2017-10-23 15:30:29 +0900

    add サーバー起動時に表示される画面を指定しました。

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('front'));
+
 app.get('/', (req, res) => {
     res.send('Hello World hoge');
 });
