c0ff4d620f57142d000ce84bf8b30530f815325d
kokikono <kk.k.0421@outlook.jp>
2017-10-23 22:25:56 +0900

    add: base api( router )

--- routers/api.js
+++ routers/api.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import logger from 'morgan';
+import selfRouter from './self_api';
 
 const router = express.Router();
+router.use(logger());
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Successfully Posted a api message.' });
+router.use('/', selfRouter);
+
+// 404 message
+router.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
 });
+
+// error handling
+router.use((err, req, res, next) => {
+    res.json({
+        message: err.message ? err.message : 'initial server error',
+        status: err.status ? err.status : 500,
+        ok: err.ok ? err.ok : false,
+    });
+})
 
 export default router;
