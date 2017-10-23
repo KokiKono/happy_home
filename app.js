d170cdcac308659239d87f23eca27a55d368d621
kokikono <kk.k.0421@outlook.jp>
2017-10-23 20:05:52 +0900

    add: api router

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import apiRouter from './routers/api';
 
 const app = express();
 const port = 8080;
@@ -8,6 +9,9 @@ app.use(express.static('front'));
 app.get('/', (req, res) => {
     res.send('Hello World hoge');
 });
+
+app.use('/api', apiRouter);
+
 
 app.listen(port, () => {
     console.log(`happy home app listening on ${port}`);
