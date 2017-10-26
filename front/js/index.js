824df72b37b73700bdcbb6ae30b0686b786f00c6
Tester <yankuma4@gmail.com>
2017-10-26 23:35:53 +0900

    add サブスクリーンレイアウトほぼおk

--- front/js/index.js
+++ front/js/index.js
@@ -1,37 +1,55 @@
-var ctx = document.getElementById("canvas");
-var myChart = new Chart(ctx, {
+(function($){
+  var ctx = document.getElementById("canvas").getContext("2d");
+  var button = "#add";
+
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+      labels: ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June"],
+      datasets: [{
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgb(255, 0, 0)',
+        data: [100,120,130,100,150,100,120],
+        type: 'line',
+      },{
+        label: '父',
+        backgroundColor: 'rgb(255, 99, 132)',
+        data:[20, 10, 5, 2, 20, 30, 45, 10, 5, 2, 20, 30, 45],
+      },{
+        label: '母',
+        backgroundColor: 'rgb(255, 99, 0)',
+        data:[30, 10, 5, 2, 20, 30, 45],
+      },{
+        label: '娘',
+        backgroundColor: 'rgb(0, 99, 132)',
+        data:[40, 10, 5, 2, 20, 30, 45],
+      },
+    ]},
+    options:{
+
+      responsive: true,
+      scales: {
+        xAxes: [{
+          stacked: true,
+        }],
+        yAxes: [{
+          stacked: true
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+      },
     }
-});
+  });
+
+  $(function(){
+    $(button).on('click',function(){
+      var max_size = chart.data.datasets[0].data.length;
+      chart.data.datasets[0].data[max_size] = Math.floor( Math.random() * (100 + 1 - 1) ) + 1 ;;
+      chart.update();
+    });
+  });
+
+  $.graph = {
+
+  }
+
+})(jQuery);
