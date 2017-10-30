70bbe6963769bd5e4a940e6192ac92c67d981b02
Tester <yankuma4@gmail.com>
2017-10-30 14:49:47 +0900

    change socket.js rename

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,11 +1,11 @@
-var Socket = function(io){
-  io.on('connection', function(socket) {
+const Socket = function (io) {
+  io.on('connection', (socket) => {
     // クライアントから受け取ったメッセージをサーバーで処理するようイベントを登録
     // （c2s→client to server | s2c→server to client）
-    socket.on('add', function(val) {
+    socket.on('add', (val) => {
       io.emit('add', val);
       console.log('動いたよ');
     });
   });
-}
+};
 module.exports = Socket;
