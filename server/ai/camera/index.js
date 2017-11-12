1cafcb4ea368712a87f50a71d8245c13db378133
kokikono <kk.k.0421@outlook.jp>
2017-11-12 12:30:21 +0900

    fix: cameraの共通化

--- server/ai/camera/index.js
+++ server/ai/camera/index.js
@@ -1,21 +1,68 @@
 import * as childProcess from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
+import Async from 'async';
+import momentTimezone from 'moment-timezone';
+import moment from 'moment';
 
-const run = (num, imagePath) => { // eslint-disable-line
-    return new Promise((resolve, reject) => { // eslint-disable-line
-        console.log(`python ${path.join(__dirname, 'camera.py')} ${num} ${imagePath}`);
-        // childProcess.exec(`python ./camera.py ${num}`, (err, stdout) => {
-        //     if (err) {
-        //         reject(err);
-        //     }
-        //     resolve(stdout);
-        // });
-        childProcess.exec(`python ${path.join(__dirname, 'camera.py')} ${num} ${imagePath}`, (err, stdout) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(stdout);
+export default class Camera {
+    constructor(imageNum, imagePath) {
+        this.imageNum = imageNum;
+        this.imagePath = imagePath;
+        const nowTime = momentTimezone().tz('Asia/Tokyo').format();
+        this.now = moment(nowTime);
+    }
+
+    /**
+     * 指定された写真を複数枚撮る。
+     * @returns {Promise}
+     */
+    take() {
+        return new Promise((resolve, reject) => { // eslint-disable-line
+            console.log(`python ${path.join(__dirname, 'camera.py')} ${this.imageNum} ${this.imagePath}`);
+            childProcess.exec(`python ${path.join(__dirname, 'camera.py')} ${this.imageNum} ${this.imagePath}`, (err, stdout) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(stdout);
+            });
         });
-    });
-};
-export default run;
+    }
+
+    fileStatus(filePath) { // eslint-disable-line
+        return new Promise((resolve, reject) => {
+            fs.stat(filePath, (err, stats) => {
+                if (err) reject(err);
+                resolve(stats);
+            });
+        });
+    }
+
+    /**
+     * インスタンス作成されてからカメラで撮った写真のfileリストを返す。
+     * @returns {Promise}
+     */
+    readCarefullySelectedImageFiles() {
+        return new Promise((resolve, reject) => {
+            fs.readdir(this.imagePath, (err, files) => {
+                if (err) reject(err);
+                let fileList = []; // eslint-disable-line
+                Async.each(files, async (file) => {
+                    // 画像ファイルのみ
+                    if (!/.*\.jpg$/.test(file)) { return; }
+                    const status = await this.fileStatus(`${this.imagePath}/${file}`).catch((err2) => { reject(err2); });
+                    const atime = moment(status.atime);
+                    const diffSec = atime.diff(this.now.format(), 'milliseconds');
+                    if (diffSec > 0) {
+                        fileList.push(`${this.imagePath}/${file}`);
+                    }
+                    // デバッグ用
+                    // fileList.push(`${this.imagePath}/${file}`);
+                }, (err3) => {
+                    if (err3) reject(err3);
+                    resolve(fileList);
+                });
+            });
+        });
+    }
+}
--- server/presentation/production/createFamilyPreparation.js
+++ server/presentation/production/createFamilyPreparation.js
@@ -2,81 +2,38 @@
  * 家族構成前準備を行う。
  * Created by kokikono on 2017/11/08.
  */
-import momentTimezone from 'moment-timezone';
-import moment from 'moment';
 import * as fs from 'fs';
 import Async from 'async';
 
-
-import camera from '../../ai/camera';
+import Camera from '../../ai/camera';
 import * as microsoftAzure from '../../ai/microsoftAzure';
 // import * as microsoftAzure from '../mock/microsoftAzure';
 import TmpFaceDao from '../models/tmpFace';
-
-// format YYYY-MM-DD:HH:mm:ss
 
 export default class createFamilyPreparation {
     constructor(imageNum, imagePath) {
         this.imageNum = imageNum;
         this.imagePath = imagePath;
-        const nowTime = momentTimezone().tz('Asia/Tokyo').format();
-        this.now = moment(nowTime);
     }
 
-    fileStatus(filePath) { // eslint-disable-line
-        return new Promise((resolve, reject) => {
-            fs.stat(filePath, (err, stats) => {
-                if (err) reject(err);
-                resolve(stats);
-            });
-        });
-    }
-
-    /**
-     * インスタンス作成されてからカメラで撮った写真のfileリストを返す。
-     * @returns {Promise}
-     */
-    readCarefullySelectedImageFiles() {
-        return new Promise((resolve, reject) => {
-            fs.readdir(this.imagePath, (err, files) => {
-                if (err) reject(err);
-                let fileList = []; // eslint-disable-line
-                Async.each(files, async (file) => {
-                    // 画像ファイルのみ
-                    if (!/.*\.jpg$/.test(file)) { return; }
-                    const status = await this.fileStatus(`${this.imagePath}/${file}`).catch((err2) => { reject(err2); });
-                    const atime = moment(status.atime);
-                    const diffSec = atime.diff(this.now.format(), 'milliseconds');
-                    if (diffSec > 0) {
-                        fileList.push(`${this.imagePath}/${file}`);
-                    }
-                    // デバッグ用
-                    // fileList.push(`${this.imagePath}/${file}`);
-                }, (err3) => {
-                    if (err3) reject(err3);
-                    resolve(fileList);
-                });
-            });
-        });
-    }
     postFaceAPIDetects(filePaths) { // eslint-disable-line
         return new Promise((resolve, reject) => {
-            let processd = 1;
+            let process = 1;
             const responseBody = [];
             Async.each(filePaths, (filePath) => {
                 microsoftAzure.postFaceDetect(fs.createReadStream(filePath))
                     .then(async (result) => {
-                        processd += 1;
+                        process += 1;
                         if (result.length > 0) {
                             responseBody.push({ result, filePath });
                         }
-                        if (processd === filePaths.length) {
+                        if (process === filePaths.length) {
                             resolve(responseBody);
                         }
                     }).catch((postErr) => {
                         console.log(postErr);
-                        processd += 1;
-                        if (processd === filePaths.length) {
+                        process += 1;
+                        if (process === filePaths.length) {
                             resolve(responseBody);
                         }
                 });
@@ -90,9 +47,11 @@ export default class createFamilyPreparation {
     start() {
         return new Promise(async (resolve, reject) => {
             // カメラ起動して複数枚写真をとる。
-            await camera(this.imageNum, this.imagePath).catch((err) => { reject(err); });
+            const camera = new Camera(this.imageNum, this.imagePath);
+            await camera.take();
+            // await camera(this.imageNum, this.imagePath).catch((err) => { reject(err); });
             // 撮った写真の取得
-            const files = await this.readCarefullySelectedImageFiles()
+            const files = await camera.readCarefullySelectedImageFiles()
                 .catch((err) => { reject(err); });
             // 撮った写真をfaceAPIに投げる
             const detects = await this.postFaceAPIDetects(files).catch((err) => { reject(err); });
