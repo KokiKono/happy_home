1e0318c93f6f47c5b7c37f6edf5453a43c31058b
kokikono <kk.k.0421@outlook.jp>
2017-11-13 15:41:34 +0900

    add: sample

---
+++ server/animation/sample/sample_animation/request.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const apiUrl = 'http://localhost:8080/event/animations';
+
+$(() => {
+	const target = $('div#target');
+
+	if (!target[0]) {
+		console.log('要素が見つかりません');
+		return;
+	}
+
+	const observer = new MutationObserver((mutationRecords, mutationObserver) => {
+		// 再生開始時
+		if (target.hasClass('anm-state-playing')) {
+			// ここに再生開始時に実行したい処理を書く
+			console.log('playing!');
+			postStatus('begin');
+		}
+		// 一時停止時
+		else if (target.hasClass('anm-state-paused')) {
+			// ここに一時停止時に実行したい処理を書く
+			console.log('paused!');
+			postStatus('pause');
+		}
+		// 再生終了時
+		else if (target.hasClass('anm-state-stopped')) {
+			// ここに再生終了時に実行したい処理を書く
+			console.log('finished!');
+			postStatus('end');
+		}
+	});
+	const config = {
+		// 対象ノードの属性の監視を有効にする
+		attributes: true
+	};
+	observer.observe(target[0] , config);
+});
+
+// 指定されたPOSTリクエストを送る
+function postStatus(status) {
+	$.ajax({
+		url: apiUrl,
+		type: 'POST',
+		dataType: 'json',
+		data: {'type': status},
+		timeout: 1000,
+	}).done((data) => {
+		console.log('POST succeed!')
+		console.log(data);
+	}).fail((XMLHttpRequest, textStatus, errorThrown) => {
+		console.log('POST failed...')
+		console.log('HttpStatus: ' + XMLHttpRequest.status)
+		console.log('TextStatus: ' + textStatus)
+		console.log('ErrorThrown: ' + errorThrown)
+	});
+}
+
+// 実行したい任意の関数
+function hoge() {
+	console.log('called hoge function!!');
+}
--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import apiRouter from './routers/api';
 import managementRouter from './routers/management';
 import socket from './socket/index';
 import Watch from './watch/index';
-
+import presentation from './presentation/mock';
 
 const app = express();
 const config = configFile[process.env.NODE_ENV];
@@ -18,8 +18,9 @@ app.socket = Socket;
 app.set('view engine', 'ejs');
 
 app.use(morgan({ format: 'dev', immediate: true }));
-app.use(express.static('views/sub'));
-
+app.use('/main', express.static('views/main'));
+app.use('/sub', express.static('views/sub'));
+app.use('/animation', express.static('animation'));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -37,3 +38,5 @@ app.use('/management', managementRouter);
 Socket.server.listen(config.server.port, () => {
     console.log(`happy home app mode is ${process.env.NODE_ENV} listening on ${config.server.port}`);
 });
+// うざいので、
+// presentation(app);
