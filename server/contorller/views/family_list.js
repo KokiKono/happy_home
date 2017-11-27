3a55d178147c439345962f2694abc33287b6a8cc
takai <urb38273745@gmail.com>
2017-11-27 16:08:34 +0900

    update: 家族構成作成画面

--- server/contorller/views/family_list.js
+++ server/contorller/views/family_list.js
@@ -22,30 +22,4 @@ router.get('/', (req, res) => {
         })
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    var i = 0;
-    var family_list = [];
-    
-    req.body.face_id.forEach((value, key) => {
-        var family = {face_id : req.body.face_id[i], type : req.body.type[i], name : req.body.name[i]};
-        family_list.push(family)
-        i++;
-    });
-    console.log(family_list);
-    console.log(JSON.stringify(family_list));
-
-    const familyListModel = new FamilyListModel();
-    familyListModel.select()
-        .then((result) => {
-            console.log(result.results);
-            res.render('main/family_list', {
-                main: result.results
-            });
-        })
-        .catch((err) => {
-            next(err);
-        })
-});
-
 export default router;
