c8d539d0cda2919d79b5080f987439c7af1a9adc
kokikono <kk.k.0421@outlook.jp>
2017-11-27 11:15:01 +0900

    add

--- server/contorller/management/index.js
+++ server/contorller/management/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by kokikono on 2017/11/25.
  */
+
+const createLink = (label, href) => ({ label, href });
+const links = [];
+links.push(createLink('ホーム', './'))
+links.push(createLink('提案判断登録', './suggestion/'));
+links.push(createLink('感情キー未登録一覧', './suggestion/judgment_key_list'));
+
+exports.index = (req, res) => {
+    res.render('management/index', {
+        links,
+    });
+};
