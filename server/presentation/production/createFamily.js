27132d2c55d77d12c045226b7999a25bc3a5bb67
kokikono <kk.k.0421@outlook.jp>
2017-12-01 15:19:34 +0900

    Add プレゼン用の家族作成モデュール作成

--- server/presentation/production/createFamily.js
+++ server/presentation/production/createFamily.js
@@ -1,3 +1,32 @@
 /**
  * Created by kokikono on 2017/11/13.
  */
+import deepEqual from 'deep-equal';
+import FamilyModel from '../models/family';
+require('waitjs');
+const pageUrl = 'http://localhost:8080/family_list';
+
+const createFamily = (app) => {
+    return new Promise(async (resolve, reject) => {
+        const familyModel = new FamilyModel();
+        // 現在のfamilyIdの最大値を取得
+        const latestFamily = await familyModel.latestFamily().catch(err => reject(err));
+        familyModel.end();
+        // main画面を家族構成画面にする。
+        app.socket.io.emit('url', pageUrl);
+        repeat('5 secs', async () => {
+            console.log('watch createFamily');
+            const nextFamilyModel = new FamilyModel();
+            const nextFamily = await nextFamilyModel.latestFamily();
+            console.log(latestFamily);
+            console.log(nextFamily);
+            if (!deepEqual(latestFamily, nextFamily)) {
+                console.log('end createFamily');
+                clear('watch_db');
+                resolve('success');
+            }
+        }, 'watch_db');
+    });
+};
+
+export default createFamily;
