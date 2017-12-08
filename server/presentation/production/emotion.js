17e3c0d494f45442c0dcc289a9f7cc26b566b3ff
kokikono <kk.k.0421@outlook.jp>
2017-12-08 17:58:08 +0900

    update: Refactoring

--- server/presentation/production/emotion.js
+++ server/presentation/production/emotion.js
@@ -11,6 +11,7 @@ import Camera from '../../ai/camera';
 import FamilyDao from '../models/family';
 
 export default class Emotion {
+
     /**
      * 複数枚の写真を撮りそれぞれのemotion情報を取得する。
      * @param imageNum
@@ -26,100 +27,104 @@ export default class Emotion {
      * @returns {Promise}
      */
     insertEmotions(familyDao, familyId, emotions) {
-        return new Promise((resolve) => {
-            let process = 1;
-            const responseBody = [];
-            emotions.forEach(async (emotion) => {
-                const emotionId = await familyDao
-                    .insertEmotion(familyId, JSON.stringify(emotion.result), emotion.filePath)
-                    .catch(err => console.log(err));
-                process += 1;
-                const emotionDetailList = [];
-                Async.each(emotion.result, (item) => {
-                    emotionDetailList.push({
-                        emotion: item,
-                        emotionId,
+        return new Promise((resolve, reject) => {
+            try {
+                let process = 1;
+                const responseBody = [];
+                return emotions.forEach(async (emotion) => {
+                    const emotionId = await familyDao
+                        .insertEmotion(familyId, JSON.stringify(emotion.result), emotion.filePath);
+                    process += 1;
+                    const emotionDetailList = [];
+                    Async.each(emotion.result, (item) => {
+                        emotionDetailList.push({
+                            emotion: item,
+                            emotionId,
+                        });
                     });
+                    responseBody.push(emotionDetailList);
+                    if (process === emotions.length) {
+                        resolve(responseBody);
+                    }
                 });
-                responseBody.push(emotionDetailList);
-                if (process === emotions.length) {
-                    resolve(responseBody);
-                }
-            });
+            } catch (err) {
+                return reject(err);
+            }
         });
     }
     start() {
         return new Promise(async (resolve, reject) => {
-            const familyDao = new FamilyDao();
-            const latestFamily = await familyDao.latestFamily().catch(err => reject(err));
-            const familyId = latestFamily[0].id;
-            console.log(`emotion start family id is ${familyId}`);
-            // MicroSoft Azure API Client
-            const faceClient = new oxford.Client(configFile['api-key'].faceAPI, configFile.azureApi.region.faceAPI);
-            const emotionClient = new oxford.Client(configFile['api-key'].emotionAPI, configFile.azureApi.region.emotionAPI);
-            // カメラを起動して複数枚写真を撮る。
-            const camera = new Camera(this.imageNum, this.imagePath);
-            await camera.take();
-            // 撮った写真の取得
-            const files = await camera.readCarefullySelectedImageFiles();
-            // 撮った写真をfaceAPIに投げる。
-            const detects = await camera.postFaceAPIDetects(faceClient.face.detect, files)
-                .catch(err => console.log(err));
-            // faceLIstからfaceIdを抽出する
-            const faceIds = [];
-            Async.each(detects, (detect) => {
-                Async.each(detect.result, (face) => {
-                    faceIds.push(face.faceId);
-                });
-            });
-            // faceIdsをグルーピング
-            const { groups } = await faceClient.face.grouping(faceIds).catch((err) => {
-                console.log(`emotion情報のfaceIds ${err}`);
-                console.log(faceIds)
-                reject(err);
-            });
-            // filesをemotionAPIに投げる。
-            const emotions = await camera.postEmotionAPI(emotionClient.emotion.analyzeEmotion, files)
-                .catch((err) => { reject(err); });
-            // emotionをDBにインサート
-            const emotionList = await this.insertEmotions(familyDao, familyId, emotions);
-            // groupsを元にdetectとemotionを結合
-            const emotionDetectGroupList = [];
-            Async.each(groups, (group) => {
-                const emotionDetectGroup = [];
-                Async.each(group, (faceId, next) => {
-                    const { result, filePath } = Camera.findDetects(faceId, detects);
-                    const partsEmotion = Camera.findEmotion(emotionList, result.faceRectangle);
-                    if (partsEmotion === undefined) {
-                        console.log('emotionとdetectの結合に失敗');
-                        next();
-                    }
-                    emotionDetectGroup.push({ detect: result, emotion: partsEmotion, filePath });
-                });
-                emotionDetectGroupList.push(emotionDetectGroup);
-            });
-            // DBに保存されている代表faceIdと同一人物性を比較し、一番近いものを代表faceIdとする。
-
-            // DBに保存している代表faceIdsを取得
-            const modelFaceIds = await familyDao.getModelFamily(familyId);
-            Async.each(emotionDetectGroupList, async (emotionDetectGroup) => {
-                const comparisonFaceId = emotionDetectGroup[0].detect.faceId;
-                const findSimilarList = await faceClient
-                    .face.similar(comparisonFaceId, { candidateFaces: modelFaceIds })
-                    .catch(err => reject(err));
-                const most = await Camera.mostFindSimilar(findSimilarList);
-                Async.each(emotionDetectGroup, async (emotionDetect) => {
-                    // const familyDao2 = new FamilyDao();
-                    await familyDao.insertindividual(
-                        emotionDetect.emotion.emotionId,
-                        JSON.stringify(emotionDetect.emotion),
-                        most.faceId,
-                    ).catch((err) => {
-                        console.log('個別感情テーブルインサートエラー', err);
+            try {
+                const familyDao = new FamilyDao();
+                const latestFamily = await familyDao.latestFamily();
+                const familyId = latestFamily[0].id;
+                console.log(`emotion start family id is ${familyId}`);
+                // MicroSoft Azure API Client
+                const faceClient = new oxford.Client(configFile['api-key'].faceAPI, configFile.azureApi.region.faceAPI);
+                const emotionClient = new oxford.Client(configFile['api-key'].emotionAPI, configFile.azureApi.region.emotionAPI);
+                // カメラを起動して複数枚写真を撮る。
+                const camera = new Camera(this.imageNum, this.imagePath);
+                await camera.take();
+                // 撮った写真の取得
+                const files = await camera.readCarefullySelectedImageFiles();
+                // 撮った写真をfaceAPIに投げる。
+                const detects = await camera.postFaceAPIDetects(faceClient.face.detect, files);
+                // faceLIstからfaceIdを抽出する
+                const faceIds = [];
+                Async.each(detects, (detect) => {
+                    Async.each(detect.result, (face) => {
+                        faceIds.push(face.faceId);
                     });
                 });
-            });
-            resolve('success');
+                // faceIdsをグルーピング
+                const { groups } = await faceClient.face.grouping(faceIds).catch((err) => {
+                    console.log(`emotion情報のfaceIds ${err}`);
+                    console.log(faceIds)
+                    reject(err);
+                });
+                // filesをemotionAPIに投げる。
+                const emotions =
+                    await camera.postEmotionAPI(emotionClient.emotion.analyzeEmotion, files);
+                // emotionをDBにインサート
+                const emotionList = await this.insertEmotions(familyDao, familyId, emotions);
+                // groupsを元にdetectとemotionを結合
+                const emotionDetectGroupList = [];
+                Async.each(groups, (group) => {
+                    const emotionDetectGroup = [];
+                    Async.each(group, (faceId, next) => {
+                        const { result, filePath } = Camera.findDetects(faceId, detects);
+                        const partsEmotion = Camera.findEmotion(emotionList, result.faceRectangle);
+                        if (partsEmotion === undefined) {
+                            console.log('emotionとdetectの結合に失敗');
+                            next();
+                        }
+                        emotionDetectGroup
+                            .push({ detect: result, emotion: partsEmotion, filePath });
+                    });
+                    emotionDetectGroupList.push(emotionDetectGroup);
+                });
+                // DBに保存されている代表faceIdと同一人物性を比較し、一番近いものを代表faceIdとする。
+
+                // DBに保存している代表faceIdsを取得
+                const modelFaceIds = await familyDao.getModelFamily(familyId);
+                Async.each(emotionDetectGroupList, async (emotionDetectGroup) => {
+                    const comparisonFaceId = emotionDetectGroup[0].detect.faceId;
+                    const findSimilarList = await faceClient
+                        .face.similar(comparisonFaceId, { candidateFaces: modelFaceIds });
+                    const most = await Camera.mostFindSimilar(findSimilarList);
+                    Async.each(emotionDetectGroup, async (emotionDetect) => {
+                        // const familyDao2 = new FamilyDao();
+                        await familyDao.insertindividual(
+                            emotionDetect.emotion.emotionId,
+                            JSON.stringify(emotionDetect.emotion),
+                            most.faceId,
+                        );
+                    });
+                });
+                return resolve('success');
+            } catch (err) {
+                return reject(err);
+            }
         });
     }
-};
+}
--- server/presentation/production/scene.js
+++ server/presentation/production/scene.js
@@ -7,26 +7,30 @@ import FamilyModel from '../models/family';
 
 const pageUrl = 'http://localhost:8080/main/select_scene';
 
-const run = (app) => {
-    return new Promise(async (resolve, reject) => {
-        const familyModel = new FamilyModel();
-        const latestFamily = await familyModel.latestFamily().catch(err => reject(err));
-        familyModel.end();
-        const presentationModel = new PresentationModel();
-        const latestScene = await presentationModel.getLatestScene(latestFamily[0].id);
-        // main画面をシーン選択にする。
-        app.socket.io.emit('url', pageUrl);
-        require('waitjs');
-        repeat('5 secs', async () => {
-            console.log('watch scene');
-            const nextScene = await presentationModel.getLatestScene(latestFamily[0].id);
-            if (!deepEqual(latestScene, nextScene)) {
-                console.log('end scene');
-                clear('watch_db');
-                resolve('sccess');
-            }
-        }, 'watch_db');
-    });
-}
+const run = app => (
+    new Promise(async (resolve, reject) => {
+        try {
+            const familyModel = new FamilyModel();
+            const latestFamily = await familyModel.latestFamily();
+            familyModel.end();
+            const presentationModel = new PresentationModel();
+            const latestScene = await presentationModel.getLatestScene(latestFamily[0].id);
+            // main画面をシーン選択にする。
+            app.socket.io.emit('url', pageUrl);
+            require('waitjs'); // eslint-disable-line
+            return repeat('5 secs', async () => { // eslint-disable-line
+                console.log('watch scene');
+                const nextScene = await presentationModel.getLatestScene(latestFamily[0].id);
+                if (!deepEqual(latestScene, nextScene)) {
+                    console.log('end scene');
+                    clear('watch_db'); // eslint-disable-line
+                    return resolve('sccess');
+                }
+            }, 'watch_db');
+        } catch (err) {
+            return reject(err);
+        }
+    })
+);
 
 export default run;
