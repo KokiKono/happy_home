5867df7df41a2e855c2aae8f6bb34ec5071b1296
kokikono <kk.k.0421@outlook.jp>
2017-12-08 11:53:15 +0900

    add: 家内アニメーション選択処理

--- server/presentation/models/presentation.js
+++ server/presentation/models/presentation.js
@@ -38,4 +38,27 @@ export default class Presentation {
             );
         });
     }
+
+    insertPattern(sceneId, pattern) {
+        return new Promise((resolve, reject) => {
+            this.connection.beginTransaction((beginTransactionErr) => {
+                if (beginTransactionErr) reject(beginTransactionErr);
+                this.connection.query(
+                    'INSERT INTO t_pattern(scene_id, pattern) VALUES(? ,?)',
+                    [sceneId, pattern],
+                    (queryErr) => {
+                        if (queryErr) {
+                            this.connection.rollback(() => (reject(queryErr)));
+                        }
+                        this.connection.commit((commitErr) => {
+                            if (commitErr) {
+                                this.connection.rollback(() => (reject(commitErr)));
+                            }
+                            resolve();
+                        });
+                    },
+                );
+            });
+        })
+    }
 }
