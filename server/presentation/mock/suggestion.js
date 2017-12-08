cb6203ff7210c4aa06a4970b02e77e157171f6b9
kokikono <kk.k.0421@outlook.jp>
2017-12-08 11:22:08 +0900

    add: LEDの検証

--- server/presentation/mock/suggestion.js
+++ server/presentation/mock/suggestion.js
@@ -2,11 +2,19 @@
  * 提案に関するmock関数
  * Created by kokikono on 2017/11/07.
  */
+import LedModel, * as LedModelConst from '../../models/led'; // eslint-disable-line
 const suggestion = () => { // eslint-disable-line
     return new Promise((resolve) => {
-        setTimeout(() => { // eslint-disable-line
-            return resolve('success');
-        }, 1000);
+        const led = new LedModel(2);
+        led.on();
+        led.setBrightness(LedModelConst.MAX_BRIGHTNESS);
+        led.setPreset(LedModelConst.PRESET_COLOR_GRADATION);
+        // setTimeout(() => {
+        //     led.pink();
+        // }, 5000);
+        // led.pink();
+        led.close();
+        resolve();
     });
 };
 
