24cb0baec1599574e08c863bda82b3591faf465e
I.Asakawa <natsu_aio@yahoo.co.jp>
2017-12-08 11:55:20 +0900

    add : ScenePatternConstant

---
+++ server/ScenePatternConstant.js
@@ -0,0 +1,21 @@
+/* シーン定数 */
+export const SCENE_FAMILY = 0;
+export const SCENE_ABSENCE = 1;
+
+/* パターン定数 */
+//家族用
+export const SCENE_FAMILY_RETURN_HOME_JOY = 1;
+export const SCENE_FAMILY_RETURN_HOME_ANGRY = 2;
+export const SCENE_FAMILY_HEARTHSTONE = 3;
+export const SCENE_FAMILY_SILENCE = 4;
+
+//留守用
+export const SCENE_ABSENCE_RETURN_HOME = 1;
+export const SCENE_ABSENCE_RONELY = 2;
+export const SCENE_ABSENCE_TRIED = 3;
+export const SCENE_ABSENCE_HAPPINESS = 4;
+
+//単体
+export const ANIMATION_COMMON = 99;
+export const PHOTO_EMOTION_READING = 111;
+export const PHOTO_TEIAN_KUN = 222;
