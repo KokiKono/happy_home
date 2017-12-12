cd142ac1791c1bc818052957756a66398b41dae0
kokikono <kk.k.0421@outlook.jp>
2017-12-12 21:12:35 +0900

    Update: 本番　途中

--- server/presentation/production.js
+++ server/presentation/production.js
@@ -18,6 +18,7 @@ import homeAnimationChoice from './production/homeAnimationChoice';
 import Suggestion from './production/suggestion';
 import suggestionPermission from './production/suggestionPermistion';
 import emotionSocket from './production/emotionSocket';
+import * as beginAnimation from './production/beginAnimation';
 
 const run = async (app) => {
 
@@ -42,47 +43,60 @@ const run = async (app) => {
     result = await scene(app).catch((err) => { throw err; });
     console.log(`シーン選択終了 ${result}`);
 
+    // console.log('帰宅中アニメーショ開始');
+    // result = await animation.start(app).catch((err) => { throw err; });
+    // console.log(`帰宅中アニメーション終了 ${result} `);
+    result = await beginAnimation.beginEmotion();
+    console.log('感情読み取り開始');
+    const emotion = new Emotion(10, path.join(__dirname, '../views/public/images/'))
+    result = await emotion.start().catch((err) => {
+        console.log('emotion err!!!!!!!1');
+        console.log(err);
+        throw err;
+    });
+    // return;
+    emotionSocket(app);
+
+
+    console.log(`感情読み取り終了 ${result}`);
+
+    console.log('帰宅中アニメーション選択処理開始');
+    result = await homeAnimationChoice().catch((err) => { throw err; });
+    console.log(`帰宅中アニメーション選択処理終了 ${result}`);
+
     console.log('帰宅中アニメーショ開始');
     const animation = new utilAnimation();
     result = await animation.start(app).catch((err) => { throw err; });
     console.log(`帰宅中アニメーション終了 ${result} `);
 
-    console.log('感情読み取り開始');
-    const emotion = new Emotion(10, path.join(__dirname, '../views/public/images/'))
-    result = await emotion.start().catch((err) => { throw err; });
-    emotionSocket(app);
-    console.log(`感情読み取り終了 ${result}`);
 
-    console.log('家内アニメーション選択処理開始');
-    result = await homeAnimationChoice().catch((err) => { throw err; });
-    console.log(`家内アニメーション選択処理終了 ${result}`);
-
-    console.log('家内アニメーショ開始');
-    result = await animation.start(app).catch((err) => { throw err; });
-    console.log(`家内アニメーション終了 ${result} `);
-
-    console.log('スマホ確認アニメーショ開始');
-    result = await animation.start(app).catch((err) => { throw err; });
-    console.log(`スマホ確認アニメーション終了 ${result} `);
+    // console.log('スマホ確認アニメーショ開始');
+    // result = await animation.start(app).catch((err) => { throw err; });
+    // console.log(`スマホ確認アニメーション終了 ${result} `);
 
     // ここがネック。どういう状態になったら終了にしていいかの判断が難しい。
+    result = await beginAnimation.beginConfirmSmtartFhone();
     console.log('スマホ操作中アニメーショ開始');
     result = await animation.start(app).catch((err) => { throw err; });
     console.log(`スマホ操作中アニメーション終了 ${result} `);
 
+    result = await beginAnimation.beginComebackHome();
     console.log('帰宅前アニメーション開始');
     result = await animation.start(app).catch((err) => { throw err; });
     console.log(`帰宅前アニメーション終了 ${result} `);
 
+    result = await beginAnimation.beginEmotion();
     console.log('感情読み取り開始');
     result = await emotion.start().catch((err) => { throw err; });
     emotionSocket(app);
     console.log(`感情読み取り終了 ${result}`);
 
+    result = await beginAnimation.beginFamilyHeartstone();
     console.log('ご飯アニメーション開始');
     result = await animation.start(app).catch((err) => { throw err; });
     console.log(`ご飯アニメーション終了 ${result} `);
 
+    result = await beginAnimation.beginSuggestion();
     console.log('提案許可処理開始');
     result = await suggestionPermission().catch((err) => { throw err; });
     console.log(`提案許可処理終了 ${result}`);
@@ -92,16 +106,29 @@ const run = async (app) => {
     result = await suggestion.start(app).catch((err) => { throw err; });
     console.log(`提案終了 ${result} `);
 
+    result = await beginAnimation.beginEmotion();
     console.log('感情読み取り開始');
     result = await emotion.start().catch((err) => { throw err; });
     emotionSocket(app);
     console.log(`感情読み取り終了 ${result}`);
 
+    // あとでちゃんとする。
+    result = await beginAnimation.beginComebackHome();
     console.log('提案結果アニメーション開始');
     result = await animation.start(app).catch((err) => { throw err; });
     console.log(`提案結果アニメーション終了 ${result} `);
 
     console.log('ストーリーシステム終了');
 };
+
+function sleep(time) {
+    const d1 = new Date();
+    while (true) {
+        const d2 = new Date();
+        if (d2 - d1 > time) {
+            return;
+        }
+    }
+}
 
 export default run;
