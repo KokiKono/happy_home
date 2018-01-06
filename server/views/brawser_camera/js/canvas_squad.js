2b2b79ba8f7111b9c5e4770c0d263f2fa787a8da
I.Asakawa <natsu_aio@yahoo.co.jp>
2018-01-06 12:56:25 +0900

    update : camera

--- server/views/brawser_camera/js/canvas_squad.js
+++ server/views/brawser_camera/js/canvas_squad.js
@@ -5,13 +5,35 @@ window.onload = function() {
     var tracker = new tracking.ObjectTracker('face');
     tracker.setInitialScale(4);
     tracker.setStepSize(2);
-    tracker.setEdgesDensity(0.1);
-    tracking.track('#video', tracker, { camera: true });
+    tracker.setEdgesDensity(0.5);
+    tracking.track('#video', tracker, {
+        camera: true
+    });
     tracker.on('track', function(event) {
         context.clearRect(0, 0, canvas.width, canvas.height);
         event.data.forEach(function(rect) {
             context.strokeStyle = '#fff';
-            context.strokeRect(rect.x+20, rect.y, rect.width, rect.height);
+            context.strokeRect(rect.x + 20, rect.y, rect.width, rect.height);
         });
     });
 };
+
+// window.onload = function() {
+//     var video = document.getElementById('video');
+//     var canvas = document.getElementById('canvas');
+//     var context = canvas.getContext('2d');
+//     var tracker = new tracking.ObjectTracker('face');
+//     tracker.setInitialScale(4);
+//     tracker.setStepSize(2);
+//     tracker.setEdgesDensity(0.5);
+//     tracking.track('#video', tracker, {
+//         camera: true
+//     });
+//     tracker.on('track', function(event) {
+//         context.clearRect(0, 0, canvas.width, canvas.height);
+//         event.data.forEach(function(rect) {
+//             context.strokeStyle = '#fff';
+//             context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+//         });
+//     });
+// };
